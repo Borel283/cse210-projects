9c748ac34eaecabe73bcb96709cec29c715e65c9
Borel283 <yaabountiful@gmail.com>
2024-04-27 23:05:20 +0000

    Prep 3 is done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // here we use a random number
+        Random randomGenerator = new Random();
+        string playAgain;
+
+        do
+        // do-while loop here
+        {
+            // Console.Write("What is the magic number? ");
+            // int magicNumber = int.Parse(Console.ReadLine());
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            // counting guess
+            int numberOfGuesses = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("Guess the magic number: ");
+                guess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+
+                if (magicNumber == guess)
+                {
+                    Console.WriteLine("You successfully guessed it!");
+                }
+                else if (Math.Abs(magicNumber - guess) <= 5)
+                {
+                    Console.WriteLine("You're close!");
+                }
+                else if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+
+            Console.WriteLine($"You made {numberOfGuesses} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        } while (playAgain.ToLower() == "yes");
     }
 }
