71ae99c4690ce4fee75697fdc8c717ed8dde8e64
Adji <168090880+Borel283@users.noreply.github.com>
2024-05-09 21:46:51 +0000

    Update Fraction.cs
    update

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,1 +1,60 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    //  Constructor initializes the fraction to 1/1
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // Constructor initializes the fraction with give top and denominator as 1
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    // Constructor initializes the fraction with given top and bottom
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        if (bottom == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        _bottom = bottom;
+    }
+
+    // Getter and Setter for private attributs
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        _bottom = bottom;
+    }
+
+   //
+}
 
