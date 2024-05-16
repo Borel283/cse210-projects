af00e562235e7c2423075145be0865f9187c53b2
Adji <168090880+Borel283@users.noreply.github.com>
2024-05-16 22:30:27 +0000

    Update Scripture Memorizer.cs

--- prove/Develop03/Scripture Memorizer.cs
+++ prove/Develop03/Scripture Memorizer.cs
@@ -1,51 +1,35 @@
 using System;
+using System.Linq;
 
-public class Scripture
+public class Word
 {
-    private readonly string reference;
-    private readonly string text;
+    private readonly string _value;
+    private bool _isHidden;
 
-    // Constructor for single verse
-    public Scripture(string reference, string text)
+    // Constructor
+    public Word(string value)
     {
-        this.reference = reference;
-        this.text = text;
+        _value = value;
+        _isHidden = false;
     }
 
-    // Constructor for verse range
-    public Scripture(string reference, string startVerse, string endVerse, string[] textArray)
+    // Property to get the word value
+    public string Value
     {
-        this.reference = reference;
-        int start = int.Parse(startVerse.Split(':')[1]);
-        int end = int.Parse(endVerse);
-        this.text = string.Join(" ", textArray[start - 1..end]);
+        get { return _value; }
     }
 
-    // Method to get the complete scripture
-    public string GetCompleteScripture()
+    // Property to get and set the hidden state
+    public bool IsHidden
     {
-        return $"{reference}: {text}";
+        get { return _isHidden; }
+        set { _isHidden = value; }
     }
 
-    // Method to hide random words in the scripture
-    public void HideRandomWords()
+    // Method to get the displayed value (either the word or underscores)
+    public string GetDisplayValue()
     {
-        List<string> words = text.Split(' ').ToList();
-        Random random = new Random();
-        int wordsToHide = random.Next(1, words.Count); // Randomly choose how many words to hide
-        for (int i = 0; i < wordsToHide; i++)
-        {
-            int index = random.Next(words.Count); // Randomly choose index of a word
-            words[index] = new string('_', words[index].Length); // Hide the word by replacing it with underscores
-        }
-        Console.Clear();
-        Console.WriteLine($"{reference}: {string.Join(" ", words)}");
-    }
-
-    // Method to check if all words are hidden
-    public bool AllWordsHidden()
-    {
-        return text.All(c => c == '_');
+        return _isHidden ? new string('_', _value.Length) : _value;
     }
 }
 
@@ -80,63 +64,56 @@ public class ScriptureReference
     }
 }
 
-
-  
-
-
-public class Word
+public class Scripture
 {
-    private readonly string _value;
-    private bool _isHidden;
+    private readonly ScriptureReference _reference;
+    private readonly List<Word> _words;
 
-    // Constructor
-    public Word(string value)
+    // Constructor for single verse
+    public Scripture(ScriptureReference reference, string text)
     {
-        _value = value;
-        _isHidden = false;
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
     }
 
-    // Property to get the word value
-    public string Value
+    // Constructor for verse range
+    public Scripture(ScriptureReference reference, string startVerse, string endVerse, string[] textArray)
     {
-        get { return _value; }
+        _reference = reference;
+        int start = int.Parse(startVerse.Split(':')[1]);
+        int end = int.Parse(endVerse);
+        _words = textArray[start - 1..end].Select(word => new Word(word)).ToList();
     }
 
-    // Property to get and set the hidden state
-    public bool IsHidden
+    // Method to get the complete scripture as a string
+    public string GetCompleteScripture()
     {
-        get { return _isHidden; }
-        set { _isHidden = value; }
+        return $"{_reference.GetReference()}: {string.Join(" ", _words.Select(word => word.Value))}";
     }
 
-    // Method to get the displayed value (either the word or underscores)
-    public string GetDisplayValue()
+    // Method to hide random words in the scripture
+    public void HideRandomWords()
     {
-        return _isHidden ? new string('_', _value.Length) : _value;
-    }
-}
-
-
-
-
-
-
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        ScriptureReference reference = new ScriptureReference("John", "3:16");
-        Scripture scripture = new Scripture(reference.GetReference(), "For God so loved the world...");
-        Console.WriteLine(scripture.GetCompleteScripture());
-        
-        while (!scripture.AllWordsHidden())
+        Random random = new Random();
+        int wordsToHide = random.Next(1, _words.Count); // Randomly choose how many words to hide
+        for (int i = 0; i < wordsToHide; i++)
         {
-            Console.WriteLine("Press enter to continue or type 'quit' to exit.");
-            string input = Console.ReadLine();
-            if (input.ToLower() == "quit")
-                break;
-            scripture.HideRandomWords();
+            int index = random.Next(_words.Count); // Randomly choose the index of a word
+            _words[index].IsHidden = true; // Hide the word
         }
+        DisplayScripture();
+    }
+
+    // Method to display the scripture with hidden words
+    public void DisplayScripture()
+    {
+        Console.Clear();
+        Console.WriteLine($"{_reference.GetReference()}: {string.Join(" ", _words.Select(word => word.GetDisplayValue()))}");
+    }
+
+    // Method to check if all words are hidden
+    public bool AllWordsHidden()
+    {
+        return _words.All(word => word.IsHidden);
     }
 }
